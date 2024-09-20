e9e295333583e196972003ce9db3d2365eccefeb
Sarvenaz12 <115059768+Sarvenaz12@users.noreply.github.com>
2024-09-21 00:09:56 +0500

    Add files via upload

---
+++ Assets/Assets/player1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+
+   public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isPressingUp = Input.GetKey(KeyCode.W);
+        bool isPressingDown = Input.GetKey(KeyCode.S);
+
+        if(isPressingUp)
+        {
+        transform.Translate(Vector2.up * Time.deltaTime); 
+
+        }
+
+        if(isPressingDown)
+        {
+        transform.Translate(Vector2.down * Time.deltaTime); 
+        
+        }
+            }
+}
